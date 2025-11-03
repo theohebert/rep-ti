459a2ce45dfd010e0c293ad184757ec8b8290e83
Théo Hebert <theo.hebert44@gmail.com>
2025-11-03 08:32:22 +0100

    ajout jinja test

---
+++ jinja_test/run_expes.py
@@ -0,0 +1,16 @@
+from jinja2 import Template
+
+tenplate_content = open('jinja_test/property_template.py.jinja').read()
+template = Template(tenplate_content)
+
+factors_comb = [
+    {"op1": "(x + y) + z", "op2": "x + (y + z)", "repetition": 10000, "seed": 0, "a": 0, "b": 1},
+    {"op1": "(x + y) + z", "op2": "x + (y + z)", "repetition": 10000, "seed": 0, "a": 1, "b": 2},
+    {"op1": "(x + y) + z", "op2": "x + (y + z)", "repetition": 100000, "seed": 0, "a": 0, "b": 1},
+    {"op1": "(x + y) + z", "op2": "x + (y + z)", "repetition": 100000, "seed": 0, "a": 1, "b": 2},
+]
+
+for factor in factors_comb:
+    generated_code = template.render(factor)
+    exec(generated_code)
+    print("\n")
