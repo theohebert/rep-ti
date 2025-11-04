f543fa6677c3bc5ec0ce61e839412f5244068f00
Isidore GOMENDY <isidore.gomendy@insa-rennes.fr>
2025-11-04 17:51:43 +0100

    rajout banking problem

---
+++ banking_problem/banking_problem_expe.py
@@ -0,0 +1,17 @@
+from jinja2 import Template
+
+tenplate_content = open('banking/property_template.py.jinja').read()
+template = Template(tenplate_content)
+
+factors_comb = [
+    {"op1": "(x + y) + z", "op2": "x + (y + z)", "repetition": 10000, "seed": 0, "a": 0, "b": 1},
+    {"op1": "(x + y) + z", "op2": "x + (y + z)", "repetition": 10000, "seed": 0, "a": 1, "b": 2},
+    {"op1": "(x + y) + z", "op2": "x + (y + z)", "repetition": 100000, "seed": 0, "a": 0, "b": 1},
+    {"op1": "(x + y) + z", "op2": "x + (y + z)", "repetition": 100000, "seed": 0, "a": 1, "b": 2},
+]
+
+for factor in factors_comb:
+    print(f"Running test with parameters: {factor}")
+    generated_code = template.render(factor)
+    exec(generated_code)
+    print("\n")
